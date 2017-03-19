df03cb870072e76ea95cd95fa6c8e7deb7c4c5b0
Will <hiimwillcho@gmail.com>
2017-03-19 15:22:34 -0700

    Initial Commit

---
+++ Simple Powerlifting/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Simple Powerlifting
+//
+//  Created by Sungjea Cho on 3/19/17.
+//  Copyright © 2017 chillmo. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
